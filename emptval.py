fe4582587968d78e885cbe0a369e3bdd68b13e45
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-24 09:00:21 -0700

    Add files via upload

---
+++ emptval.py
@@ -0,0 +1,13 @@
+str = "Aneeq Younas "
+int = 552575
+float = 45.765
+tuple = ("john", 22, 534, 55, "David", 12)
+dict = {"Python ", 3.9}
+list = [21,23432,54645,3454574,3453234,6456323,254645764,253453,2534,345]
+
+print("This is a String: \n ", (type(str)()))
+print("This is a Integer: \n ", (type(int)()))
+print("This is a Float: \n ", (type(float)()))
+print("This is a Tuple: \n ", (type(tuple)()))
+print("This is a Dictionary: \n ", (type(dict)()))
+print("This is a List: \n ", (type(list)()))
